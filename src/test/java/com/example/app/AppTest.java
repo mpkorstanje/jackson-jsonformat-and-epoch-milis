324e32e2b74ffe21c194bdb241a5983130d48a5d
M.P. Korstanje <rien.korstanje@gmail.com>
2023-01-09 18:59:26 +0100

    More context

--- src/test/java/com/example/app/AppTest.java
+++ src/test/java/com/example/app/AppTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
@@ -15,6 +16,8 @@ class AppTest {
 
   final ObjectMapper mapper = new ObjectMapper()
       .disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS)
+      .disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
+      .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
       .setTimeZone(TimeZone.getTimeZone("UTC"))
       .findAndRegisterModules();
   final Instant instant = Instant.ofEpochMilli(123456);
@@ -26,11 +29,18 @@ class AppTest {
     assertEquals(instant, mapper.readValue("{\"time\":123456}", TimeHolderWithFormat.class).time);
     assertEquals(instant, mapper.readValue("{\"time\":\"123456\"}", TimeHolderWithFormat.class).time);
   }
+  @Test
+  public void test2() throws JsonProcessingException {
+    assertEquals("\"1970-01-01T00:02:03.456Z\"", mapper.writeValueAsString(instant));
+  }
 
   static class TimeHolder {
     private Instant time;
     public void setTime(Instant time) {
       this.time = time;
+    }
+    public Instant getTime() {
+      return time;
     }
   }
 
@@ -39,6 +49,9 @@ class AppTest {
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     public void setTime(Instant time) {
       this.time = time;
+    }
+    public Instant getTime() {
+      return time;
     }
   }
 }
